a6a8f6936b546c1d24b16d3aad63980ed2bd541c
Chandan Gupta Bhagat <gchandaniw@gmail.com>
2018-09-25 08:49:25 +0545

    Initial Commit

---
+++ ITNepal.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITNepal.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
